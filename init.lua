7a37ebdc722fe0b7caf07ec08582af541ccae945
Nikita Pikalov <n.pikalov@of-group.ru>
2025-12-08 22:41:46 +0400

    Initial Neovim configuration with lazy.nvim

---
+++ init.lua
@@ -0,0 +1,18 @@
+-- Basic
+require('core.plugins')
+require('core.mappings')
+require('core.colors')
+require('core.config')
+-- Plagins
+--require('plugins.neotree')
+--require('plugins.treesitter')
+--require('plugins.lsp')
+--require('plugins.cmp')
+--require('plugins.mason')
+--require('plugins.telescope')
+--require('plugins.formatting')
+--require('plugins.gitsigns')
+--require('plugins.leap')
+--require('plugins.helm')
+--require('plugins.kube-utils')
+
